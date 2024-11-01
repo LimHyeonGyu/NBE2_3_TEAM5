c0b797d0d5801113ec904e2454fe0b9479ab210e
chadoli27 <jasminekim1009@gmail.com>
2024-11-01 14:40:44 +0900

    securityUtil 관련 코드들 추가

--- src/main/kotlin/edu/example/dev_3_5_cc/dto/board/BoardRequestDTO.kt
+++ src/main/kotlin/edu/example/dev_3_5_cc/dto/board/BoardRequestDTO.kt
@@ -1,10 +1,21 @@
 package edu.example.dev_3_5_cc.dto.board
 
+import edu.example.dev_3_5_cc.entity.Board
 import edu.example.dev_3_5_cc.entity.Category
+import edu.example.dev_3_5_cc.entity.Member
 
 data class BoardRequestDTO (
     var memberId: String? = null,
     var title: String? = null,
     var description: String? = null,
     var category: Category = Category.GENERAL
-)
+){
+    fun toEntity(member : Member): Board {
+        return Board(
+            member = member,
+            title = this.title,
+            description = this.description,
+            category = this.category
+        )
+    }
+}
