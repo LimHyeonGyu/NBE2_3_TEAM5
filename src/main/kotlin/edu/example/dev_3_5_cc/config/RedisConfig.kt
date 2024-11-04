7941c1e2717e52a2a9723002e3df9f7edae73a6c
LimHyeunGyu <delldro94@gmail.com>
2024-11-04 23:20:09 +0900

    Feat: add RedisConfig

---
+++ src/main/kotlin/edu/example/dev_3_5_cc/config/RedisConfig.kt
@@ -0,0 +1,29 @@
+package edu.example.dev_3_5_cc.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+@EnableCaching  // 캐싱 기능 활성화
+class RedisConfig(
+    @Value("\${spring.data.redis.host:localhost}") private val host: String,
+    @Value("\${spring.data.redis.port:6379}") private val port: Int
+) {
+    //Redis 서버와의 연결 설정
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    // Redis 서버와의 데이터를 주고 받기 위한 RedisTemplate 생성
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = redisConnectionFactory()
+        return template
+    }
+}
