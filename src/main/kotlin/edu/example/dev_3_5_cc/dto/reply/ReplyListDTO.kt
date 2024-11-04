f41e818668115234704d6fa796eebdeaa85bdc58
LimHyeunGyu <delldro94@gmail.com>
2024-11-05 00:17:35 +0900

    Feat: DTO에 Serializable 추가2

--- src/main/kotlin/edu/example/dev_3_5_cc/dto/reply/ReplyListDTO.kt
+++ src/main/kotlin/edu/example/dev_3_5_cc/dto/reply/ReplyListDTO.kt
@@ -1,13 +1,14 @@
 package edu.example.dev_3_5_cc.dto.reply
 
 import edu.example.dev_3_5_cc.entity.Reply
+import java.io.Serializable
 
 data class ReplyListDTO (
     var replyId : Long? = null,
     var memberId : String? = null,
     var boardId : Long? = null,
     var parentReplyId: Long? = null, // 부모 댓글 ID 추가
-){
+): Serializable {
     constructor(reply: Reply) : this(
         replyId = reply.replyId,
         memberId = reply.member?.memberId,
